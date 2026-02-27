32e0947655fe6b7499abe192e4bff5e7c3100d8f
José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>
2026-02-27 16:32:56 +0400

    first commit

---
+++ sanity/env.js
@@ -0,0 +1,4 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-02-27";
+export const dataset = process.env.SANITY_STUDIO_DATASET;
+export const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
---
+++ sanity/schemaTypes/authorType.js
@@ -0,0 +1,59 @@
+import {UserIcon} from '@sanity/icons'
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export const authorType = defineType({
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  icon: UserIcon,
+  fields: [
+    defineField({
+      name: 'name',      
+      title: 'Name of the author',
+      type: 'string',
+       validation: (Rule) => Rule.required(),
+       validation: (Rule) => Rule.min(2).max(50),
+    }),
+    defineField({
+      name: 'lastName',      
+      title: 'Last name(s) of the author',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'string',
+        }),
+      ],  
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'bio',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+        }),
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+})
